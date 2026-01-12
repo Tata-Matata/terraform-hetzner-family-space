d67c37e991468e165305dacb5e64eda06e63a8ef
Tatiana Nikolaeva <tatiana.nikolaeva@gmx.net>
2026-01-12 09:27:45 +0100

    Bastion consumes core network outputs

--- bastion/main.tf
+++ bastion/main.tf
@@ -1,31 +1,6 @@
-data "terraform_remote_state" "network" {
-  //CHANGE to Terraform Cloud 
-  backend = "local"
-
-  config = {
-    path = "../core-network/terraform.tfstate"
-  }
-}
-
-/* 
-data "terraform_remote_state" "tf_state_network" {
- 
-  backend = "remote"
-
-  config = {
-    hostname     = "app.terraform.io"
-    organization = "tatamatata-org"
-
-    workspaces = {
-      name = "core-network"
-    }
-  }
-} */
-
-
 module "bastion" {
-  source     = "../modules/bastion_vm"
-  //CHANGE to Terraform Cloud
-  network_id = data.terraform_remote_state.network.outputs.id
+  source     = "../modules/bastion"
+  private_network_cidr = data.terraform_remote_state.core_network.outputs.subnet_cidr
+  ssh_public_key = var.ssh_public_key
 }
 
---
+++ bastion/variables.tf
@@ -0,0 +1,10 @@
+variable "hcloud_token" {
+  sensitive = true
+}
+
+variable "ssh_public_key" {
+  description = "Admin SSH public key used for bootstrap access to bastion host"
+  default = file("~/.ssh/id_ed25519.pub")
+}
+
+
