2cb498ff492fcb2f5c310cec2d57f998ca26da53
Tatiana Nikolaeva <tatiana.nikolaeva@gmx.net>
2026-01-12 09:40:03 +0100

    Bastion host IP derived from network CIDR

--- bastion/main.tf
+++ bastion/main.tf
@@ -1,6 +1,7 @@
 module "bastion" {
   source     = "../modules/bastion"
-  private_network_cidr = data.terraform_remote_state.core_network.outputs.subnet_cidr
+  subnet_cidr = data.terraform_remote_state.core_network.outputs.subnet_cidr
   ssh_public_key = var.ssh_public_key
+  subnet_id = data.terraform_remote_state.core_network.outputs.subnet_id
 }
 
