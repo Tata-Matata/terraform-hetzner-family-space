f14aca64fa9505e5fbd60be23a453a16d75150af
Tatiana Nikolaeva <tatiana.nikolaeva@gmx.net>
2026-01-12 09:49:06 +0100

    Init ssh key for bastion access

--- bastion/main.tf
+++ bastion/main.tf
@@ -1,7 +1,7 @@
 module "bastion" {
   source     = "../modules/bastion"
   subnet_cidr = data.terraform_remote_state.core_network.outputs.subnet_cidr
-  ssh_public_key = var.ssh_public_key
+  ssh_public_key_path = var.ssh_public_key_path
   subnet_id = data.terraform_remote_state.core_network.outputs.subnet_id
 }
 
--- bastion/variables.tf
+++ bastion/variables.tf
@@ -2,9 +2,9 @@ variable "hcloud_token" {
   sensitive = true
 }
 
-variable "ssh_public_key" {
+variable "ssh_public_key_path" {
   description = "Admin SSH public key used for bootstrap access to bastion host"
-  default = file("~/.ssh/id_ed25519.pub")
+  default = "~/.ssh/id_ed25519.pub"
 }
 
 
