7c9150ff3775093e73d86eb45402e986a9363ed5
Tatiana Nikolaeva <tatiana.nikolaeva@gmx.net>
2026-01-17 17:16:20 +0100

    Ansible inventory

---
+++ ansible_inventory/outputs.tf
@@ -0,0 +1,13 @@
+resource "local_file" "ansible_inventory" {
+  filename = "../../ansible/inventory/hosts.yaml"
+
+  content = templatefile(
+    "${path.module}/templates/inventory.yaml.tftpl",
+    {
+      consul_private_ips   = data.terraform_remote_state.consul.outputs.consul_private_ips
+      vault_private_ips    = data.terraform_remote_state.vault.outputs.vault_private_ips
+      k8s_controlplane_ips = data.terraform_remote_state.k8s.outputs.control_plane_private_ips
+      k8s_worker_ips       = data.terraform_remote_state.k8s.outputs.worker_private_ips
+    }
+  )
+}
--- consul/outputs.tf
+++ consul/outputs.tf
@@ -1,12 +1,19 @@
-output "consul_private_ip" {
-  value = module.consul_server.server_private_ip
+output "consul_private_ips" {
+  value = [
+    for server in module.consul_server : server.server_private_ip
+  ]
 }
 
-output "consul_server_id" {
-  value = module.consul_server.server_id
+
+output "consul_server_ids" {
+  value = [
+    for server in module.consul_server : server.server_id
+  ]
 }
 
 output "consul_server_labels" {
-  value = module.consul_server.server_labels
+  value = [
+    for server in module.consul_server : server.server_labels
+  ]
 
 }
